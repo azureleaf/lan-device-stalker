bb3666eec9c12a555c4562a33b481da8903a6f81
akb <doge.wow.so.scare@gmail.com>
2020-05-08 20:20:13 +0900

    feat: Save the scan result to SQLite DB with SQLAlchemy

--- arp-scan.py
+++ arp-scan.py
@@ -1,6 +1,10 @@
 import subprocess
 import psutil
 import re
+import socket
+import sqlalchemy as db
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+from datetime import datetime
 
 
 def get_interface():
@@ -33,24 +37,55 @@ def format_arp_scan_result(scan_result):
     scan_result = re.sub(r'\\t', r'\t', scan_result)
     scan_result = re.sub(r'\\n', r'\n', scan_result)
 
-    # convert multiline str into list
+    now = str(datetime.now())
+
+    # convert multiline str into list,
+    # then extract the lines of IP addrs & MAC addrs
     devices = []
     for line in scan_result.splitlines():
-
-        # extract lines which has IP data, thereby remove unnecessary lines
-        if re.match(r'192', line) is not None:
-            # convert tab-deliminated items into array,
-            #   then remove whitespaces
-            device = [x.strip() for x in line.split("\t")]
-            devices.append(device)
+        # convert tab-deliminated items into array,
+        # then remove whitespaces
+        words = [x.strip() for x in line.split("\t")]
+        if len(words[0].split(".")) == 4:
+            # check if the string has the valid IPv4 format
+            try:
+                socket.inet_aton(words[0])
+                devices.append({
+                    "ip_addr": words[0],
+                    "mac_addr": words[1],
+                    "created_at": now
+                })
+            except socket.error:
+                continue
 
     return devices
+
+
+def save_to_db(new_devices, db_path):
+    engine = create_engine(db_path)
+    conn = engine.connect()
+    meta = MetaData(engine)
+    devices = Table('devices', meta,
+                    Column('id', Integer, primary_key=True),
+                    Column('ip_addr', String),
+                    Column('mac_addr', String),
+                    Column('created_at', String)
+                    )
+    if not engine.dialect.has_table(engine, "devices"):
+        meta.create_all(engine)
+        print("DB created.")
+
+    query = db.insert(devices)
+    ResultProxy = conn.execute(query, new_devices)
+
+    results = conn.execute(db.select([devices])).fetchall()
+    return results
 
 
 def wrapper():
     interface = get_interface()
     devices = get_devices(interface)
-    print(devices)
+    save_to_db(devices, "sqlite:///devices.db")
 
 
 if __name__ == "__main__":
