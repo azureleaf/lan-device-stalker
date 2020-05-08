114918be2d7373b82d96b887c173bca2b4230cf7
akb <doge.wow.so.scare@gmail.com>
2020-05-09 00:27:10 +0900

    feat: Summarize the ARP-SCAN records, collect timestamps of every device

--- arp-scan.py
+++ arp-scan.py
@@ -8,6 +8,9 @@ from datetime import datetime
 
 
 def get_interface():
+    '''
+    Scan network interfaces, return name of the active valid one
+    '''
     addrs = psutil.net_if_addrs()
     interfaces = addrs.keys()
     for interface in interfaces:
@@ -17,8 +20,9 @@ def get_interface():
 
 
 def get_devices(interface):
-    """ Get list of devices on the LAN, then record it to database
-    """
+    '''
+    Get list of devices on the LAN, then record it to database
+    '''
 
     # run shell command of this computer
     cmd = subprocess.Popen(
@@ -32,6 +36,9 @@ def get_devices(interface):
 
 
 def format_arp_scan_result(scan_result):
+    '''
+    Parse the result text of the arp-scan, return it as a dict
+    '''
 
     # turn plain string \t \n into actual tab & line break
     scan_result = re.sub(r'\\t', r'\t', scan_result)
@@ -61,31 +68,71 @@ def format_arp_scan_result(scan_result):
     return devices
 
 
-def save_to_db(new_devices, db_path):
+def save_to_db(new_devices, db_path, devices):
     engine = create_engine(db_path)
     conn = engine.connect()
     meta = MetaData(engine)
-    devices = Table('devices', meta,
-                    Column('id', Integer, primary_key=True),
-                    Column('ip_addr', String),
-                    Column('mac_addr', String),
-                    Column('created_at', String)
-                    )
+
     if not engine.dialect.has_table(engine, "devices"):
         meta.create_all(engine)
         print("DB created.")
 
     query = db.insert(devices)
-    ResultProxy = conn.execute(query, new_devices)
-
-    results = conn.execute(db.select([devices])).fetchall()
-    return results
+    conn.execute(query, new_devices)
 
 
-def wrapper():
-    interface = get_interface()
-    devices = get_devices(interface)
-    save_to_db(devices, "sqlite:///devices.db")
+def summarize_db(db_path, devices):
+    """
+    Check the records of devices responded to ARP-SCAN,
+    return occurences summary of every device
+    """
+    engine = create_engine(db_path)
+    conn = engine.connect()
+
+    # Fetch all the mac addresses
+    query = db.select([devices.columns.mac_addr.distinct()]
+                      ).order_by(db.desc(devices.columns.mac_addr))
+    mac_addrs = conn.execute(query).fetchall()
+
+    # Get occurences of every device
+    devices_occs = []  # Occurences of all the devices
+    for mac_addr in mac_addrs:
+        query = db.select([devices]) \
+            .where(devices.columns.mac_addr == mac_addr.mac_addr) \
+            .order_by(db.desc(devices.columns.created_at))
+        # Occurence of this device
+        device_occs = conn.execute(query).fetchall()
+
+        devices_occs.append({
+            "mac_addr": mac_addr.mac_addr,
+            "occ": [device_occ.created_at for device_occ in device_occs]
+        })
+
+    return devices_occs
+
+
+def wrapper(isScanMode=False):
+    db_path = "sqlite:///devices.db"
+
+    # define the table
+    engine = create_engine(db_path)
+    meta = MetaData(engine)
+    devices_table = Table('devices', meta,
+                          Column('id', Integer, primary_key=True),
+                          Column('ip_addr', String),
+                          Column('mac_addr', String),
+                          Column('created_at', String)
+                          )
+
+    if isScanMode is True:
+        interface = get_interface()
+        devices = get_devices(interface)
+        save_to_db(devices, db_path, devices_table)
+        return
+
+    summary = summarize_db(db_path, devices_table)
+    print(summary)
+    return summary
 
 
 if __name__ == "__main__":
