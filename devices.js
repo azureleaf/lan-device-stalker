ed1305fab00ab9ef92f567ca6fd09aa0335235ba
akb <doge.wow.so.scare@gmail.com>
2020-06-10 12:11:29 +0900

    feat: Show sample mac address occurences with D3.js

---
+++ devices.js
@@ -0,0 +1,12 @@
+const deviceHists = [
+  {
+    group: "2020-06-10 11:30:01.982251",
+    variable: "a0:b0:c0:d0:e0:f0",
+    value: "1",
+  },
+  {
+    group: "2020-06-10 12:00:01.982251",
+    variable: "a1:b0:c0:d0:e0:f0",
+    value: "1",
+  },
+];
